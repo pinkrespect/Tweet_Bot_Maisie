098cf0c379496ef09f78383211512b8fd0d92d41
pinkrespect <miss.sonagi@gmail.com>
2017-02-21 02:32:43 +0900

    Change library Tweepy & Try to get Error Code and Message

--- Twitter_Bot.py
+++ Twitter_Bot.py
@@ -1,26 +1,14 @@
-# URL 읽어온다.
-from urllib.request import urlopen
-# 트위터 API
-from twitter import *
+import tweepy
+from Twitter_auths import *
+
+auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
+auth.set_access_token(ACCESS_TOKEN_KEY, ACCESS_TOKEN_SECRET)
+
+api = tweepy.API(auth)
 
 try:
-    from Twitter_auths import *
-except:
-    print('Tweet API Key are not exist.')
-    exit()
-
-t = Twitter(auth=OAuth(CONSUMER_KEY,
-CONSUMER_SECRET,
-ACCESS_TOKEN_KEY,
-ACCESS_TOKEN_SECRET))
-
-message = '아 ㅡㅡ 이거 참 좋은데 트위터라이브러리가 너무 많아서.. 짜증난다..'
-
-# 트위터 함수를 정의했다.
-def tweet(message):
-    t.statuses.update(status=message)
-
-try:
-    tweet(message)
-except:
-    tweet(message*2)
+    api.update_status('아 왜 안올라가 ㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋ')
+except tweepy.TweepError as Err:
+    print(Err)
+    print(type(Err))
+## How to use Err to Duplicated Error(no. 187)

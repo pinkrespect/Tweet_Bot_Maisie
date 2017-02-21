83ec0fb59b60822e2834dd043811a52790c89777
pinkrespect <miss.sonagi@gmail.com>
2017-02-21 15:02:42 +0900

    check Error code

--- Twitter_Bot.py
+++ Twitter_Bot.py
@@ -6,9 +6,15 @@ auth.set_access_token(ACCESS_TOKEN_KEY, ACCESS_TOKEN_SECRET)
 
 api = tweepy.API(auth)
 
+
 try:
+    status = tweepy.API.home_timeline()
     api.update_status('아 왜 안올라가 ㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋ')
 except tweepy.TweepError as Err:
-    print(Err)
-    print(type(Err))
-## How to use Err to Duplicated Error(no. 187)
+    print(Err.api_code)
+    if Err.api_code == 187:
+        tweets = '이건 중복 트윗이라 오류난 것임(' + str(Err.api_code) + ')'
+        api.update_status(tweets)
+    else:
+        tweets ='이건 '+ str(Err.api_code) +'번 때문에 오류난것임'
+        api.update_status(tweets)

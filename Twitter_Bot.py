9316554e3f6c47de59d742dd6cabe197166ce8af
Didee <miss.sonagi@gmail.com>
2017-02-22 16:21:57 +0900

    StreamListener 완성ㅇ0ㅇ)/

--- Twitter_Bot.py
+++ Twitter_Bot.py
@@ -1,34 +1,40 @@
-import tweepy, time
-from Twitter_auths import *
-from Twitter_Bot_Defs import *
+import tweepy
+import sys
+from tweepy.streaming import StreamListener
+from tweepy import Stream
+import time
+from Twitter_auths import CONSUMER_KEY, CONSUMER_SECRET, ACCESS_TOKEN_KEY, ACCESS_TOKEN_SECRET, ADMIN_SCREEN_NAME
 from Twitter_Bot_Errors import *
+from Twitter_Bot_Defs import *
 
 auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
 auth.set_access_token(ACCESS_TOKEN_KEY, ACCESS_TOKEN_SECRET)
 
-api = tweepy.API(auth)
-my_name = '@' + api.me().screen_name.lower() # my_name = str
+api = tweepy.API(auth, wait_on_rate_limit =True, wait_on_rate_limit_notify=True)
+my_name = '@' + api.me().screen_name.lower()
+# my_name = str
 # api.me().screen_name is sometimes doesn't fit who mentioned lower or upper case screen name.
 
-while(1):
-    try:
+class Timeline_Listener(StreamListener):
+    def on_status(self, status):
         mentions = api.mentions_timeline()
         URLs_dict, follow_yet_dict = Mention(mentions, my_name)
-        if (URLs_dict is not None) and (follow_yet_dict is not None):
-             for key in URLs_dict:
-                print('@' + str(URLs_dict[key][0]) + " " + str(URLs_dict[key][1]))
-                api.update_status('@' + str(URLs_dict[key][0]) + " " + str(URLs_dict[key][1]) + str(URLs_dict[key][2]), key)
+        if URLs_dict is not None:
+            for key in URLs_dict:
+                string = '@' + str(URLs_dict[key][0]) + " " + str(URLs_dict[key][1]) + "\n검색어: " + str(URLs_dict[key][2])
+                api.update_status(string, key)
                 api.create_favorite(key)
+        #if follow_yet_dict is not None:
+        #    pass
 
-        else:
-            print('sleep')
-            time.sleep(20)
-            print('wakeup')
+    def on_error(self, status_code):
+        api.update_status(Error_Codes(status_code))
+        return False
 
-    except tweepy.TweepError as Err:
-        api.update_status(Error_Codes(Err.api_code)) #봇 주것을때 타임스탬프 ㅠ
-        pass
+def Run_Bot():
+    while True:
+        listener = Timeline_Listener()
+        stream = tweepy.Stream(auth = auth, listener = listener)
+        stream.userstream(_with = 'user', async = False)
 
-    except Unexceptable_Error:
-        api.update_status(Error_Codes(000) + ADMIN_SCREEN_NAME)
-        pass
+Run_Bot()
--- Twitter_Bot_Defs.py
+++ Twitter_Bot_Defs.py
@@ -1,28 +1,22 @@
+""" 여기 있는 함수들은 멘션에 쓰일 메세지를 생성하거나, 멘션을 보낼지 말지에 대한 판단을 합니다."""
 from urllib.parse import quote
-
-def return_dict(integer, dict1, string1, string2, string3):
-    mentioned_data_array = [string1, string2, string3]
-    dict1[integer] = {integer:mentioned_data_array} 
-    return dict1
-
-# new_text 양식: (검색 사이트) (검색할 단어)
 
 def Mention(mentions, my_name):
     mentioned_data_dict = {}
     follow_yet_dict = {}
     for mention in mentions:
         new_text = mention.text.lower()
-        if (new_text.find(my_name) >= 0) or (mention.favorited == False):
+        if (my_name in new_text) or (mention.favorited is False):
             start_index, end_index = My_Name_Index(new_text, my_name)
             new_text = mention.text.replace(mention.text[start_index:end_index], "")
 
-            if (((new_text.find('네이버') or new_text.find('구글')) >= 0) and (new_text.find("팔로우") or new_text.find("팔로"))>= 0):
+            if (("네이버" or "구글") and ("팔로")) in new_text:
                 mentioned_data_dict[mention.id] = [mention.user.screen_name, new_text]
 
-            elif (new_text.find('네이버') and new_text.find('구글')) >= 0:
+            elif "네이버" or "구글" in new_text:
                 mentioned_data_dict[mention.id] = [mention.user.screen_name, new_text]
 
-            elif (new_text.find("팔로우") and new_text.find("팔로")) >= 0:
+            elif "팔로" in new_text:
                 follow_yet_dict[mention.id] = [mention.user.screen_name, new_text]
 # 추후 여기에 else 구문 고민 해 볼것
             URLs_dict = {}
@@ -33,37 +27,36 @@ def Mention(mentions, my_name):
             return None, None
 
 def My_Name_Index(new_text, my_name):
-    if new_text.find(my_name + " ") <= 0:
+    if my_name + " " in new_text:
         My_name_len = len(my_name)+1
-    elif new_text.find(my_name) <= 0:
+    elif my_name in new_text:
         My_name_len = len(my_name)
 
     start_index = new_text.find(my_name)
-    end_index = new_text.find(my_name) + My_name_len
+    end_index = start_index + My_name_len
     return start_index, end_index
-    
+
 
 def Make_URLs(mentioned_data_dict):
     URLs_dict = {}
     for number in mentioned_data_dict.keys():
-        if mentioned_data_dict[number][1].find("네이버") >= 0:
+        if "네이버" in mentioned_data_dict[number][1]:
             start_index, end_index = My_Name_Index(mentioned_data_dict[number][1], '네이버')
             search_word = mentioned_data_dict[number][1].replace(mentioned_data_dict[number][1][start_index:end_index], "")
+            original_word = search_word##
             search_word = quote(search_word, safe='')
-            URLs_dict[number][2] = search_word##
             search_word = search_word.replace("%20", "+")
             search_word = 'http://search.naver.com/search.naver?query=' + search_word
-            URLs_dict[number] = [ mentioned_data_dict[number][0], search_word ]
+            URLs_dict[number] = [mentioned_data_dict[number][0], search_word, original_word]
 
-        if mentioned_data_dict[number][1].find("구글") >= 0:
+        if "구글" in mentioned_data_dict[number][1]:
             start_index, end_index = My_Name_Index(mentioned_data_dict[number][1], '구글')
             search_word = mentioned_data_dict[number][1].replace(mentioned_data_dict[number][1][start_index:end_index], "")
+            original_word = search_word##
             search_word = quote(search_word, safe='')
-            URLs_dict[number][2] = search_word##
             search_word = search_word.replace("%20", "+")
             search_word = 'http://www.google.com/search?q=' + search_word
-            URLs_dict[number] = [ mentioned_data_dict[number][0], search_word,  ]
-
+            URLs_dict[number] = [mentioned_data_dict[number][0], search_word, original_word]
     return URLs_dict
 
 def Follow(follow_yet_dict):

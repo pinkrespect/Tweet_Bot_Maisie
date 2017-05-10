057ae7626e3a8cc5878eb0644b1a618f0f81c950
Maisie <miss.sonagi@gmail.com>
2017-05-10 18:44:52 +0900

    Implement part of class Timeline.(almost Add Comment)

--- new/maisie.py
+++ new/maisie.py
@@ -12,7 +12,7 @@ from tweepy.streaming import StreamListener
 
 from auth import C_KEY, C_SECRET, A_TOKEN_KEY, A_TOKEN_SECRET
 # from errors import Error_codes
-import defs
+from defs import mention
 
 
 # When It imported as module, It raise Keyboard Interrupt error.
@@ -40,4 +40,40 @@ ME = API.me()
 MY_NAME = '@'+(ME.screen_name).lower()
 
 print(" * BOT NAME: " + MY_NAME + " * ")
+
+
+class Timeline(StreamListener):
+    """Timeline class.
+
+    tweepy.StreamLitsener
+    Listen Twitter bot's timeline.
+    """
+    
+    def on_status(self, status):
+        """on_status in class Timeline.
+
+        status listener.
+        override tweepy.on_status()
+        """
+
+        mentions = API.mentions_timeline()  # Twitter GET API
+        """API.mentions_timeline()
+
+        Returns the 20 most recent mentions, including retweets.
+
+        API.mentions_timeline([since_id ][, max_id ][, count])
+
+        Parameters
+            • since_id – Returns only statuses with an ID greater than
+            (that is, more recent than) the specified ID.
+            • max_id – Returns only statuses with an ID less than
+            (that is, older than) or equal to the specified ID.
+            • count – Specifies the number of statuses to retrieve.
+            Return type list of Status objects
+
+        From Tweepy Doc.
+        """
+
+        # def mention() in defs.py
+        URLS, FOLLOWS = mention(mentions, MY_NAME)
 
